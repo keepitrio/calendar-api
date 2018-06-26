f29bf0b76c1809108c2f3f4b43018e09198c1aa3
Rio <rio.nose@smartsparrow.com>
2018-06-25 23:26:36 -0400

    Allow editing event

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -10,6 +10,21 @@ module Api::V1
             render json: @event
         end
 
+        def update
+            @event = Event.find(params[:id])
+            @event.update_attributes(event_params)
+            render json: @event
+        end
+
+        def destroy
+            @event = Event.find(params[:id])
+            if @event.destroy
+                head :no_content, status: :ok
+            else
+                render json: @event.errors, status: :unprocessable_entity
+            end
+        end
+
         private
 
         def event_params
